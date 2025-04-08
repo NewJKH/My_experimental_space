8e1c6d84af3efe247f330182444378a75b8fad8e
NewJKH <newsun0805@gmail.com>
2025-04-08 16:34:38 +0900

    충돌

--- src/progremmers/level0/ArraySum.java
+++ src/progremmers/level0/ArraySum.java
@@ -1,19 +1,12 @@
 package progremmers.level0;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.Arrays;
 
 public class ArraySum {
 
-    public void test(String[] s1, String[] s2){
-        int a = s1.length;
-        int b = s2.length;
-
-        int sum = a+b;
-        Set<String> s3 = new HashSet<>();
-        s3.addAll(List.of(s1));
-        s3.addAll(List.of(s2));
-        s3.size();
+    public int[] test(int n, int[] numlist){
+        return Arrays.stream(numlist)
+                .filter(num -> num % n == 0)
+                .toArray();
     }
 }
