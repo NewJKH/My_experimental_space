f188c58c0f353915d5b61ee04e469cca69f5d684
NewJKH <newsun0805@gmail.com>
2025-04-15 13:04:07 +0900

    commit merge

--- src/anonymous/AnonyTest.java
+++ src/anonymous/AnonyTest.java
@@ -1,4 +1,13 @@
 package anonymous;
 
 public class AnonyTest {
+    public void start(){
+    Animal dog = new Animal() {
+        @Override
+        public void bark() {
+            System.out.println("멍멍");
+        }
+    };
+    dog.bark();
+    }
 }
