14fb7f77f27c54176318702f4d28f14b2b8056d3
NewJKH <newsun0805@gmail.com>
2025-06-24 09:04:29 +0900

    update: function Interface

--- src/progremmers/level1/Test.java
+++ src/progremmers/level1/Test.java
@@ -1,27 +1,44 @@
 package progremmers.level1;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+
 public class Test {
     public static void main(String[] args) {
-//        System.out.println("args = " +solution(new String[]{"ayaye", "uuu", "yeye", "yemawoo", "ayaayaa"}));
-        System.out.println("args = " + solution(new int[]{2, 1, 1, 2, 3, 1, 2, 3, 1}));
-        System.out.println("args = " + solution(new int[]{1, 3, 2, 1, 2, 1, 3, 1, 2}));
+        System.out.println("args = " + Arrays.toString(solution("2022.05.19", new String[]{"A 6", "B 12", "C 3"}, new String[]{"2021.05.02 A", "2021.07.01 B", "2022.02.19 C", "2022.02.20 C"})));
+        System.out.println("args = " + Arrays.toString(solution("2020.01.01", new String[]{"Z 3", "D 5"}, new String[]{"2019.01.01 D", "2019.11.15 Z", "2019.08.02 D", "2019.07.01 D", "2018.12.28 Z"})));
     }
 
-    public static int solution(int[] ingredient) {
-        // 빵 - 야채 - 고기 - 빵  | 1 2개 , 2 1개 , 3 1개
-        StringBuilder stack = new StringBuilder();
-        int count = 0;
-
-        for (int i : ingredient) {
-            stack.append(i);
-
-            if (stack.length() >= 4 &&
-                    stack.substring(stack.length() - 4).equals("1231")) {
-                stack.setLength(stack.length() - 4);
-                count++;
-            }
+    public static int[] solution(String today, String[] terms, String[] pri) {
+        Map<Character, Integer> times = new HashMap<>();
+        for ( String t : terms ){
+            char rank = t.charAt(0);
+            String month = t.substring(2);
+            times.put(rank, Integer.valueOf(month));
         }
+        List<Integer> list = new ArrayList<>();
+        LocalDate now = formatTime(today);
 
-        return count;
+        for ( int i = 0 ; i< pri.length;i++ ){
+            char rank = pri[i].charAt(11);
+            int month = times.get(rank);
+
+            String date = pri[i].substring(0,10);
+            LocalDate localDate = formatTime(date);
+            LocalDate newDate = localDate.plusMonths(month);
+            if ( newDate.isBefore(now) || newDate.isEqual(now) ){
+                list.add(i+1);
+            }
+
+        }
+        int[] answer = new int[list.size()];
+        for ( int n = 0; n<list.size(); n++ ){
+            answer[n] = list.get(n);
+        }
+        return answer;
+    }
+    private static LocalDate formatTime(String date){
+        return LocalDate.parse(date, DateTimeFormatter.ofPattern("yyyy.MM.dd"));
     }
 }
