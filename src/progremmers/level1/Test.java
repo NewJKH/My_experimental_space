058b78888c89eee65fb443c05bf5cd579b47dfc1
NewJKH <newsun0805@gmail.com>
2025-06-25 19:27:10 +0900

    update: function Interface

--- src/progremmers/level1/Test.java
+++ src/progremmers/level1/Test.java
@@ -1,44 +1,30 @@
 package progremmers.level1;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.*;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("args = " + Arrays.toString(solution("2022.05.19", new String[]{"A 6", "B 12", "C 3"}, new String[]{"2021.05.02 A", "2021.07.01 B", "2022.02.19 C", "2022.02.20 C"})));
-        System.out.println("args = " + Arrays.toString(solution("2020.01.01", new String[]{"Z 3", "D 5"}, new String[]{"2019.01.01 D", "2019.11.15 Z", "2019.08.02 D", "2019.07.01 D", "2018.12.28 Z"})));
+        System.out.println("args = " + "");
     }
 
-    public static int[] solution(String today, String[] terms, String[] pri) {
-        Map<Character, Integer> times = new HashMap<>();
-        for ( String t : terms ){
-            char rank = t.charAt(0);
-            String month = t.substring(2);
-            times.put(rank, Integer.valueOf(month));
-        }
-        List<Integer> list = new ArrayList<>();
-        LocalDate now = formatTime(today);
+    public String[] solution(String[] players, String[] callings) {
+        Map<String,Integer> map = new HashMap<>();
+        // 이름이 아니라 처음부터 이름 자체가 순번으로 저장했다면 어떨까?
+        // 이름을 찾는다면 시간초과가 오래 걸린다면 처음부터 이름이 아닌 숫자를 저장해놓고 숫자만 바뀌게 저장
 
-        for ( int i = 0 ; i< pri.length;i++ ){
-            char rank = pri[i].charAt(11);
-            int month = times.get(rank);
+        // 1. 즉시즉시 바뀌게 하지않고 카운팅을 사용해서 푼다면? - 시도 결과 : 순서가 안맞아서 실패
+        // 2. 컬렉션의 swap 기능을 사용한다면?
 
-            String date = pri[i].substring(0,10);
-            LocalDate localDate = formatTime(date);
-            LocalDate newDate = localDate.plusMonths(month);
-            if ( newDate.isBefore(now) || newDate.isEqual(now) ){
-                list.add(i+1);
-            }
+        List<String> list = new ArrayList<>(Arrays.asList(players));
+        for (String calling : callings) {
+            int index = list.indexOf(calling);
+            Collections.swap(list,index-1,index);
+        }
+        String[] ranks = new String[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            ranks[i] = list.get(i);
+        }
 
-        }
-        int[] answer = new int[list.size()];
-        for ( int n = 0; n<list.size(); n++ ){
-            answer[n] = list.get(n);
-        }
-        return answer;
-    }
-    private static LocalDate formatTime(String date){
-        return LocalDate.parse(date, DateTimeFormatter.ofPattern("yyyy.MM.dd"));
+        return ranks;
     }
 }
---
+++ src/startkot/StartKotlin.kt
@@ -0,0 +1,14 @@
+package startkot
+
+class StartKotlin {
+    companion object {
+        @JvmStatic
+        fun main(n:Int, t:Int):Int {
+            var abc = n
+            for ( i in 0..t){
+                abc*=2
+            }
+            return abc
+        }
+    }
+}
