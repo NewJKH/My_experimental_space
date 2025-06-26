1a45a7ab08cc9c9a625b4e847e0ba2013920b632
NewJKH <newsun0805@gmail.com>
2025-06-26 13:53:08 +0900

    update: function Interface

--- src/startkot/Main.kt
+++ src/startkot/Main.kt
@@ -1,9 +1,23 @@
 package startkot
 
 fun main() {
-    if ( true ){
-        println("조건문 사용법 ")
-    }else{
-        println("2")
+//    val kScore = readLine()!!.toInt()
+//    val eScore = readLine()!!.toInt()
+//    val mScore = readLine()!!.toInt()
+//
+//    val avg = (kScore+eScore+mScore)/3;
+//    if ( avg > 90 ){
+//        println(" = 조건 검사 90점 초과")
+//    }else if ( avg > 50 ){
+//        println(" = 조건 검사 50점 초과")
+//    }else{
+//        println(" = 조건 검사 그 아래 ")
+//    }
+
+    var infos = arrayOf("이게 리스트?"," 엄청 간단하네", "자바는 라이브러리 추가했는데 ")
+    for ( info in infos ){
+        println("info = $info")
     }
+
+
 }
