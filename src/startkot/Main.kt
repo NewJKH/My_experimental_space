48afa6bc917dc62933598c1479a527e456c5f72e
NewJKH <newsun0805@gmail.com>
2025-06-26 14:37:48 +0900

    update: function Interface

--- src/startkot/Main.kt
+++ src/startkot/Main.kt
@@ -14,10 +14,19 @@ fun main() {
 //        println(" = 조건 검사 그 아래 ")
 //    }
 
-    var infos = arrayOf("이게 리스트?"," 엄청 간단하네", "자바는 라이브러리 추가했는데 ")
-    for ( info in infos ){
-        println("info = $info")
-    }
+//    var infos = arrayOf("이게 리스트?"," 엄청 간단하네", "자바는 라이브러리 추가했는데 ")
+//    for ( info in infos ){
+//        println("info = $info")
+//    }
 
+//    val value = "12345"
+//    println("값 = "+(value+5))
+//
+//    val it = value.toInt()
+//    println("값 = "+(it+5))
 
+    var t = sum(1,3);
+}
+fun sum(va1:Int, va2:Int): Int{
+    return va1+va2
 }
