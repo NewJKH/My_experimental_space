a0fb021c89ebc186f7875b78e1b5afc200e42e2c
NewJKH <newsun0805@gmail.com>
2025-06-28 15:41:14 +0900

    update: function Interface

--- src/progremmers/level1/Test.java
+++ src/progremmers/level1/Test.java
@@ -1,30 +1,93 @@
 package progremmers.level1;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("args = " + "");
+        System.out.println("args = " + Arrays.toString(solution(new String[]{"OSO", "OOO", "OXO", "OOO"}, new String[]{"E 2", "S 3", "W 1"})));
+        System.out.println("args = " + Arrays.toString(solution(new String[]{"SOO", "OXX", "OOO"}, new String[]{"E 2", "S 2", "W 1"})));
     }
 
-    public String[] solution(String[] players, String[] callings) {
-        Map<String,Integer> map = new HashMap<>();
-        // 이름이 아니라 처음부터 이름 자체가 순번으로 저장했다면 어떨까?
-        // 이름을 찾는다면 시간초과가 오래 걸린다면 처음부터 이름이 아닌 숫자를 저장해놓고 숫자만 바뀌게 저장
+    public static int[] solution(String[] park, String[] routes) {
+        List<Move> moveList = initMoveList(routes);
+        int[] start = new int[2];
 
-        // 1. 즉시즉시 바뀌게 하지않고 카운팅을 사용해서 푼다면? - 시도 결과 : 순서가 안맞아서 실패
-        // 2. 컬렉션의 swap 기능을 사용한다면?
-
-        List<String> list = new ArrayList<>(Arrays.asList(players));
-        for (String calling : callings) {
-            int index = list.indexOf(calling);
-            Collections.swap(list,index-1,index);
-        }
-        String[] ranks = new String[list.size()];
-        for (int i = 0; i < list.size(); i++) {
-            ranks[i] = list.get(i);
+        for (int i = 0; i < park.length; i++) {
+            String s = park[i];
+            for (int j = 0; j < s.length(); j++) {
+                char c = s.charAt(j);
+                if ( c == 'S' ) {
+                    start[0] = i;
+                    start[1] = j;
+                }
+            }
         }
 
-        return ranks;
+
+        for (Move move : moveList) {
+            char dir = move.getOp();
+            int amount = move.getMove();
+
+            int ny = start[0];
+            int nx = start[1];
+            boolean blocked = false;
+
+            for (int step = 1; step <= Math.abs(amount); step++) {
+                int dy = 0, dx = 0;
+                if (dir == 'E' || dir == 'W') dx = Integer.signum(amount);
+                else dy = Integer.signum(amount);
+
+                ny += dy;
+                nx += dx;
+
+                if (ny < 0 || ny >= park.length || nx < 0 || nx >= park[0].length()) {
+                    blocked = true;
+                    break;
+                }
+
+                if (park[ny].charAt(nx) == 'X') {
+                    blocked = true;
+                    break;
+                }
+            }
+
+            if (!blocked) {
+                start[0] = ny;
+                start[1] = nx;
+            }
+        }
+        return start;
+    }
+
+    private static List<Move> initMoveList(String[] routes) {
+        List<Move> list = new ArrayList<>();
+        for (String route : routes) {
+            char op = route.charAt(0);
+            int move = Integer.parseInt(route.substring(2));
+            if ( op == 'W' || op == 'N' ) move*=-1;
+
+            list.add(new Move(op,move));
+        }
+        return list;
+    }
+
+    static class Move {
+        char op;
+        int move;
+
+        public Move(char op, int move) {
+            this.op = op;
+            this.move = move;
+        }
+
+        public char getOp() {
+            return op;
+        }
+
+        public int getMove() {
+            return move;
+        }
     }
 }
